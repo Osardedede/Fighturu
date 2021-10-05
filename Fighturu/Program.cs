cf438098d099e3ea743c4c37fb3db223c0d00b9c
Oscar Jansson <oscar.jansson121@outlook.com>
2021-10-05 11:09:04 +0200

    wegjhd

---
+++ Fighturu/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+Random generator = new Random();
+int damage = generator.Next();
+
+List<string> bossNames = new List<string>();
+bossNames.Add("Herbert");
+bossNames.Add("Josefin");
+bossNames.Add("Pikachu");
+
+int num = generator.Next(bossNames.Count);
+
+Console.WriteLine(bossNames[num]);
+
+string boss = bossNames[num];
+
+
+string name;
+stuff.titleCard();
+Console.WriteLine("Whats your name warior?");
+name = Console.ReadLine();
+
+Console.WriteLine($"Ok {name} your challanger today is {boss}");
+if (boss == "Pikachu")
+{
+    stuff.PP();
+}
+
+if (boss == "Herbert")
+{
+    stuff.BB();
+}
+Console.ReadLine();
+
+
+
+
+
+
